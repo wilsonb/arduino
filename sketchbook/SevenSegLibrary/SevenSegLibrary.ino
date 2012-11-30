ee14e4b19b5ea63806f713ca86b36ba616ccd306
Brian Wilson <wilsonb@outlook.com>
2012-11-29 22:06:37 -0800

    Add example

--- sketchbook/SevenSegLibrary/SevenSegLibrary.ino
+++ sketchbook/SevenSegLibrary/SevenSegLibrary.ino
@@ -1,32 +1,42 @@
+/*
+  Seven Segment Display Library Example
+  
+  Library works with SN74HC595 Shift Registers
+  driving a specified number of Seven Segment
+  Displays.  Default value is 4.
+  
+  Suggested pinout is in SevenSeg.h
+  
+  Created by Brian Wilson November 2012
+
+*/
+
 #include <SevenSeg.h>
 
-SevenSeg seven;
-int output[4] = { 10,10,10,10 };
+SevenSeg seven; //Initialize library
+int output[4] = { 10,10,10,10 }; //10 makes display blank
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
+  //Setup of Seven Segment Displays is in the Library files
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
+  // Counts each digit to 9 and increases the digit to the left 
    
-  for (int i1 = 0; i1 < 10; i1++) {
+  for (int i1 = 0; i1 < 10; i1++) {  
     for (int i2 = 0; i2 < 10; i2++) {
       for (int i3 = 0; i3 < 10; i3++) {
         for (int i4 = 0; i4 < 10; i4++) {
 
-          output[0] = i1;
-          output[1] = i2;
-          output[2] = i3;
-          output[3] = i4;
+          output[0] = i1; //First Digit
+          output[1] = i2; //Second Digit
+          output[2] = i3; //Third Digit
+          output[3] = i4; // Fourth Digit
           seven.sevenSegWrite(output);
-          delay(10);
+          delay(100); //100 ms delay
         }
       }
     }
-  }
-    
-  
+  }  
   
 }
